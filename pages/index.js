6f228510f6445fc52deb35e3d8c2018dd0b13c6f
Mpiranha <chukwubuezeokeke@gmail.com>
2019-04-07 15:28:57 +0100

    Update

--- pages/index.js
+++ pages/index.js
@@ -6,19 +6,15 @@ import fetch from 'isomorphic-unfetch';
 
 const Index = (props) => (
     <div className="container">
-      <h1>Todo's</h1>
+     
       <App/>
-    
+      <h1>Todo's</h1>
       <ul>
-        {/* {props.items.map(show => (
-          <li key={show.id}>
-            <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
-              <a>{show.name}</a>
-            </Link>
+        {props.items.map(todo => (
+          <li key={todo.id}>
+              {todo.item}
           </li>
-        ))} */
-        props.items
-        }
+        ))}
       </ul>
     </div>
   )
@@ -33,14 +29,14 @@ Index.getInitialProps = async function() {
   // fetch('./array.json')
   // .then(res => res.json())
   // .then(data => console.log(data));
-  const res = await fetch('https://drive.google.com/file/d/15nsWAlSIkBpRj_L9KViqaJTLB3Cgyb-w/view?usp=sharing');
-  const data = await res.text();
+  const res = await fetch('https://raw.githubusercontent.com/Mpiranha/todo-app/feature/okeke_chukwubueze/add-express-server/pages/array.json');
+  const data = await res.json();
 
   console.log(`Show data fetched. Count: ${data.length}`);
   console.log(`Show data fetched. Count: ${data}`);
 
   return {
-    items: data//map(entry => entry.todos)
+    items: data.map(entry => entry.todos)
   }
 }
 
